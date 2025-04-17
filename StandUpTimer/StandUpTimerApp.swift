6e0ce36a711681630635bece431a3be8f7fdcf09
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2025-04-17 06:28:32 +0600

    project init

---
+++ StandUpTimer/StandUpTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandUpTimerApp.swift
+//  StandUpTimer
+//
+//  Created by Shah Md Imran Hossain on 17/4/25.
+//
+
+import SwiftUI
+
+@main
+struct StandUpTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
